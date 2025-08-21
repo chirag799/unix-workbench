bc98db13097df9a345283ea3d48a09adec7e7975
chirag799 <44178235+chirag799@users.noreply.github.com>
2025-08-21 14:20:13 +0530

    Update Test.scala

--- Test.scala
+++ Test.scala
@@ -1,82 +1,188 @@
-package com.citi.purgeFramework.datawriter
 
-import com.citi.purgeFramework.utils.AppLogger
-import com.citi.purgeFramework.utils.FileUtils.{listAllFilesFolderInDir, listAllFolderInDir}
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.spark.sql.functions.{col, mean}
+package com.citi.purgeFramework.driver
 
-import java.util.Date
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import com.citi.purgeFramework.utils.AuditIntegration.{getAuditConfigDetails, logger}
+import com.citi.purgeFramework.utils.{AppLogger, AuditIntegration, SendEmail}
+import com.typesafe.config.ConfigException.Missing
+import com.typesafe.config.ConfigFactory
 
-import scala.collection.immutable.Map
-import scala.collection.mutable.ListBuffer
-import scala.util.control.Breaks._
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
+object Driver extends AppLogger with Serializable {
 
-class ParquetFileWriter(spark: SparkSession) extends AppLogger with Serializable {
-
-
-	def writemultilevelpartition(df:DataFrame,files_list:Array[String], partition_keys: Set[String],partition_groupby:String,base_path: String,output_path: String): Unit = {
-
-		val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
-		//val df = spark.sql(s"show partitions ${db_table}")
-		//fs.listFiles( new Path( path ), true )
-		//val filenames = ListBuffer[ String ]( )
-		//while (files.hasNext ) filenames += files.next().getPath().toString()
-		//filenames.foreach(println(_))
-		//val sublevel_partitions = df.select(partition_groupby).distinct()  // to get states in a df
-		//val sublevel_partition_list = sublevel_partitions.rdd.map(x=>x(0)).collect.toList //to get states in a list
-		//--------------------*********-------------------//
-
-		var filePaths = new ListBuffer[Path]()
-		val nbrkeys = partition_keys.size
-		var increment = 0
-		var pathArray = listAllFolderInDir(spark, base_path, true, filePaths).distinct
-		for (path_prin <- pathArray) { println(path_prin)}
-		var data = new ListBuffer[(String, Long)]
-		breakable {
-			for (path <- pathArray) {
-				increment = increment + 1
-				var pathSplit = path.toString().split("/")
-				for( path1 <- pathSplit) {println(path1)}
-				var pathSplitSize = pathSplit.size
-				var basepath_partition = base_path
-				for (i <- nbrkeys to 1 by -1) {
-					basepath_partition = basepath_partition + "/" + pathSplit(pathSplitSize - i)
-				}
-				val MEGABYTE: Long = 1024L * 1024L
-				val folder_size = fs.getContentSummary(new Path(basepath_partition)).getLength / MEGABYTE
-				val last_partition_level = pathSplit(pathSplitSize - 1)
-				data += ((last_partition_level, folder_size))
-				logger.info("last_partition_level, folder_size" + (last_partition_level, folder_size))
-				//var processFolderName = target_path + "/" + pathSplit(pathSplitSize - 2)
-				logger.info(path)
-				logger.info(folder_size)
-				if (increment > 500) break // break out of the for loop
-				//fs.rename(new Path(rawFileName), new Path(processFileName))
-			}
-			import spark.implicits._
-			var DF = spark.sparkContext.parallelize(data).toDF("last_partition_level","folder_size")
-			DF = DF.groupBy("last_partition_level").agg(mean(col("folder_size")).as("folder_size") )
-			DF.show(false)
-			val sublevel_partitions = DF.select(col("last_partition_level")).distinct()  // to get states in a df
-			val sublevel_partition_list = sublevel_partitions.rdd.map(x=>x(0)).collect.toList //to get states in a list
+  var audit_param_dict = scala.collection.mutable.Map[String, Any]()
+  var autosys_job_name = ""
+  var audit_int_flg = ""
+  var audit_config = ""
 
 
+  def main(sparkargs: Array[String]) : Unit  = {
+    val spark_args = sparkargs.map(_.split("=") match { case Array(x, y) => (x, y) }).toMap
+    val user_config = ConfigFactory.parseFile(new File(spark_args("user_config").split("/").last))
+    val env = spark_args("env") //Execution environment. Ex: Dev/uat/prod
 
-			for (sublevel_partition <- sublevel_partition_list)  //loop to get each state
-			{
-				logger.info("sublevel_partition : " + (sublevel_partition.toString splitAt (sublevel_partition.toString lastIndexOf "=") + 1)._2)
-				df.show(false)
-				val filtered_df = df.filter(col(partition_groupby) === (sublevel_partition.toString splitAt (sublevel_partition.toString lastIndexOf "=") +1)._2)
-				filtered_df.show(false)
-				filtered_df.printSchema()
-				var folder_size = DF.filter($"last_partition_level" === sublevel_partition).select("folder_size").collect().map(_.getDouble(0).ceil).mkString("")
-				val repartition_size = (folder_size.toDouble/512).ceil.toInt
-				logger.info("repartition_size : " + repartition_size)
-				val columns = partition_keys.toSeq.map(x => col(x))
-				filtered_df.coalesce(repartition_size).write.partitionBy((partition_keys.toSeq):_*).mode("overwrite").parquet(base_path + "_temp")
-			}
-		}
-	}
+    var err_mail_sub = try { user_config.getString("mail_config.err_mail_sub")} catch { case _: Missing => "" }
+    val sent_to = user_config.getString("mail_config.send_to_".concat(env))
+    var Input_Table_query = user_config.getString("Input.Input_table")
+    var reference_table = user_config.getString("Input.Reference_table")
+    var runAtATime = user_config.getString("Input.runAtATime")
+    val Log_table_query = user_config.getString("Input.Log_table_query")
+    var Customer_ID = user_config.getString("Input.Customer_Identification")
+    var LKP_CUSTOMER_ID = user_config.getString("Input.LKP_CUSTOMER_ID")
+    val LOG_table = user_config.getString("Input.LOG_table")
+    var Join_Query = user_config.getString("Input.Join_Query")
+    val extra_cols = try {
+      user_config.getString("Input.extra_cols")
+    } catch {
+      case _: Exception =>
+        ""
+    }
+    audit_int_flg = try {
+      user_config.getString("Datalens_Integration_Details.Datalens_Integration_Flag").toLowerCase
+    } catch {
+      case _: Exception =>
+        "false"
+    }
+    if (audit_int_flg == "true") {
+      audit_config = if (user_config.hasPath("Datalens_Integration_Details.Datalens_user_config_".concat(env))) {
+        user_config.getString("Datalens_Integration_Details.Datalens_user_config_".concat(env))
+      } else if (user_config.hasPath("Datalens_Integration_Details.Datalens_user_config")) {
+        user_config.getString("Datalens_Integration_Details.Datalens_user_config")
+      } else {
+        ""
+      }
+    }
+    logger.info("audit_config: " + audit_config)
+    var Reference_Query = user_config.getString("Input.Reference_Query")
+
+    var hdfs_for_temp_data = user_config.getString("log_paths.hdfs_path_for_temp_data")
+    val Reconciliation_table = user_config.getString("Input.Reconciliation_table")
+    var Input_table_bkp_path = user_config.getString("Input.Input_table_bkp_path")
+    val purge_status_table = "gcganamrswp_work.ccpa_purge_status"
+    val purge_mode=spark_args("purge_mode")
+    val CSI_ID=spark_args("CSI_ID")
+    val ARG_GROUP_ID=spark_args("ARG_GROUP_ID")
+    val ARG_TYPE_OF_PURGE=spark_args("ARG_TYPE_OF_PURGE")
+    val ARG_PARTITION_NAME=spark_args("ARG_PARTITION_NAME")
+    val LKP_DB_NAME=spark_args("LKP_DB_NAME")
+    val LKP_TABLE_NAME=spark_args("LKP_TABLE_NAME")
+    val LH_IND=spark_args("LH_IND")
+
+
+    //val ARG_CUSTOMER_ID=${ARG_CUSTOMER_ID}
+    val ARG_AGING=spark_args("ARG_AGING")
+    val increment_column=spark_args("increment_column")
+    val increment_datatype=spark_args("increment_datatype")
+    val TARGET_TABLE_DATE_FORMAT=spark_args("ARG_TARGET_TABLE_DATE_FORMAT")
+    val purge_date=spark_args("purge_date")
+    val column_mapping=user_config.getString("Input.Column_mapping").split(",")
+    var lits_override = try { spark_args("lits_override")} catch { case exception: Exception => "false" }
+
+    audit_param_dict = audit_param_dict + ("audit_config" -> audit_config, "env" -> env, "sent_to" -> sent_to, "lits_override" -> lits_override, "ARG_PARTITION_NAME" -> ARG_PARTITION_NAME, "increment_column" -> increment_column,
+      "increment_datatype" -> increment_datatype, "TARGET_TABLE_DATE_FORMAT" -> TARGET_TABLE_DATE_FORMAT, "LH_IND" -> LH_IND)
+
+
+    val Identifier_Type = spark_args("Identifier_Type")
+
+
+    var jobApplicationName = user_config.getString("spark.jobApplicationName")
+    var LOB = user_config.getString("spark.LOB")
+    var Input_Table = "<ARG_DB_NAME>.<ARG_TABLE_NAME>"
+
+    for ((k, v) <- spark_args) {
+      Input_Table_query = Input_Table_query.replaceAll("<" + k + ">", v)
+      Input_Table = Input_Table.replaceAll("<" + k + ">", v)
+      reference_table = reference_table.replaceAll("<" + k + ">", v)
+      Customer_ID = Customer_ID.replaceAll("<" + k + ">", v)
+      LKP_CUSTOMER_ID = LKP_CUSTOMER_ID.replaceAll("<" + k + ">", v)
+      jobApplicationName = jobApplicationName.replaceAll("<" + k + ">", v)
+      err_mail_sub = err_mail_sub.replaceAll("<" + k + ">", v)
+      Input_table_bkp_path = Input_table_bkp_path.replaceAll("<" + k + ">", v)
+      hdfs_for_temp_data = hdfs_for_temp_data.replaceAll("<" + k + ">", v)
+      runAtATime = runAtATime.replaceAll("<" + k + ">", v)
+    }
+
+    val column_mapping_lkp = column_mapping.map("lkp.<" + _ + ">")
+    val column_mapping_reference = column_mapping.map("reference_<" + _ + ">")
+    val column_mapping_target = column_mapping.map("target_table.<" + _ + ">")
+    val map_value = scala.collection.mutable.Map[String,String]()
+    val s2List = Customer_ID.split(",").map("target_table." + _ )
+    val s3List = LKP_CUSTOMER_ID.split(",").map("lkp." + _ )
+    val reference_collist = LKP_CUSTOMER_ID.split(",").map("reference_" + _ )
+
+
+    logger.info("column_mapping_lkp: " + column_mapping_lkp.mkString(" , "))
+    logger.info("column_mapping_reference: " + column_mapping_reference.mkString(" , "))
+    logger.info("column_mapping_target: " + column_mapping_target.mkString(" , "))
+
+    logger.info("s3List: " + s3List.mkString(" , "))
+    logger.info("s2List: " + s2List.mkString(" , "))
+
+
+    for (i <- 0 to column_mapping_lkp.length-1) {
+      map_value += column_mapping_lkp(i).trim() -> s3List(i).trim()}
+    for (i <- 0 to column_mapping_reference.length-1) {
+      map_value += column_mapping_reference(i).trim() -> reference_collist(i).trim()}
+    for (i <- 0 to column_mapping_target.length-1) {
+      map_value += column_mapping_target(i).trim() -> s2List(i).trim()}
+
+    logger.info(map_value.map(pair => pair._1+"="+pair._2).mkString("?","&",""))
+
+
+    for ((k, v) <- map_value) {
+      Join_Query = Join_Query.replaceAll(k, v)
+    }
+    for ((k,v) <- map_value) {
+    Reference_Query = Reference_Query.replaceAll(k, v)}
+
+    logger.info("Join_Query: " + Join_Query)
+
+    val other_user_provided_arguments = spark_args.-("audit_integration_flag").-("ARG_TYPE_OF_PURGE").-("user_config").-("ARG_AGING").-("LKP_TABLE_NAME").-("ARG_GROUP_ID").-("Identifier_Type").-("ARG_DB_NAME").-("purge_mode").-("increment_datatype").-("env").-("ARG_TABLE_NAME").-("runAtATime").mapValues(_.toString)
+
+    audit_param_dict = audit_param_dict+("input_sql"-> Input_Table_query, "target_table" -> Input_Table, "other_user_provided_arguments" -> other_user_provided_arguments, "reference_table" -> reference_table, "Customer_ID" -> Customer_ID , "LKP_CUSTOMER_ID" -> LKP_CUSTOMER_ID, "LKP_DB_NAME" -> LKP_DB_NAME, "LKP_TABLE_NAME" -> LKP_TABLE_NAME)
+
+
+    var argsList: List[Array[String]] = List[Array[String]]()
+
+    var args: Array[String] = null
+    args = Array("appName", jobApplicationName,
+      "LOB", LOB,
+      "Input_Table_query", Input_Table_query,
+      "Input_Table", Input_Table,
+      "reference_table", reference_table,
+      "Reconciliation_table", Reconciliation_table,
+      "purge_status_table", purge_status_table,
+      "Customer_ID",Customer_ID,
+      "LKP_CUSTOMER_ID",LKP_CUSTOMER_ID,
+      "Join_Query", Join_Query,
+      "extra_cols", extra_cols,
+      "Reference_Query", Reference_Query,
+      "LOG_table",LOG_table,
+      "runAtATime", runAtATime,
+      "purge_mode", purge_mode,
+      "CSI_ID", CSI_ID,
+      "LITS_DT",purge_date,
+      "ARG_GROUP_ID",ARG_GROUP_ID,
+      "ARG_TYPE_OF_PURGE",ARG_TYPE_OF_PURGE,
+      "ARG_PARTITION_NAME",ARG_PARTITION_NAME,
+      "ARG_AGING",ARG_AGING,
+      "increment_column",increment_column,
+      "increment_datatype",increment_datatype,
+      "TARGET_TABLE_DATE_FORMAT",TARGET_TABLE_DATE_FORMAT,
+      "Identifier_Type",Identifier_Type,
+      "Log_table_query",Log_table_query,
+      "lits_override", lits_override,
+      "env", env,
+      "sent_to", sent_to,
+      "hdfs_for_temp_data", hdfs_for_temp_data,
+      "Input_table_bkp_path", Input_table_bkp_path)
+    argsList = args :: argsList
+
+
+    val orchestrator = new Orchestrator
+    orchestrator.setUp(args)
+    orchestrator.processInput(args)
+    }
 }
