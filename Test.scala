ed6c63f3fe657ad9ba7193ef18abcce3406ffcae
chirag799 <44178235+chirag799@users.noreply.github.com>
2025-08-21 14:17:36 +0530

    Update Test.scala

--- Test.scala
+++ Test.scala
@@ -1,60 +1,82 @@
 package com.citi.purgeFramework.datawriter
 
-import com.citi.purgeFramework.utils.{AppLogger, AuditIntegration}
-import org.apache.spark.SparkContext
+import com.citi.purgeFramework.utils.AppLogger
+import com.citi.purgeFramework.utils.FileUtils.{listAllFilesFolderInDir, listAllFolderInDir}
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.sql.functions.{col, mean}
 
-import java.util
+import java.util.Date
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-import scala.util.{Failure, Success, Try}
+import scala.collection.immutable.Map
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks._
 
-class DataWriter(sparkSession:SparkSession) extends AppLogger with Serializable {
+
+class ParquetFileWriter(spark: SparkSession) extends AppLogger with Serializable {
 
 
-	def writeDataStaging(inputDataFrame: DataFrame): Boolean = {
+	def writemultilevelpartition(df:DataFrame,files_list:Array[String], partition_keys: Set[String],partition_groupby:String,base_path: String,output_path: String): Unit = {
 
-		logger.info("Inside writeDataStaging")
+		val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+		//val df = spark.sql(s"show partitions ${db_table}")
+		//fs.listFiles( new Path( path ), true )
+		//val filenames = ListBuffer[ String ]( )
+		//while (files.hasNext ) filenames += files.next().getPath().toString()
+		//filenames.foreach(println(_))
+		//val sublevel_partitions = df.select(partition_groupby).distinct()  // to get states in a df
+		//val sublevel_partition_list = sublevel_partitions.rdd.map(x=>x(0)).collect.toList //to get states in a list
+		//--------------------*********-------------------//
 
-		//val deltaTableWriter = new DeltaFileWriter()
-		// Check if table exits of not and write routine to generate the tables.
-		//new DeltaFileWriter().write(targetTableDefinition,inputDataFrame)
-		true
-	}
-
-	val start = 300000
-	val end = 500000
-	val rnd = new scala.util.Random
-
-	def hiveInsert(data_df: DataFrame, spark: SparkSession, sc: SparkContext, hiveDB: String, Outputhive_table: String): Unit = {
-		//This function will re-try inserting into hive table for a max of three times
-		val insrt_countr = sc.longAccumulator("Hive insert attempt counter")
-		var insertFlag: Boolean = false
-		//insrt_countr.add(1)
-		while (insrt_countr.value <= 3 && !insertFlag) {
-			Try {
-				insrt_countr.add(1)
-				logger.info("Inside hiveInsert routine for inserting values into  -----> " + hiveDB + "." + Outputhive_table + "insrt_countr value: " + insrt_countr)
-				spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
-				data_df.coalesce(1).write.mode("append").insertInto(hiveDB + "." + Outputhive_table)
+		var filePaths = new ListBuffer[Path]()
+		val nbrkeys = partition_keys.size
+		var increment = 0
+		var pathArray = listAllFolderInDir(spark, base_path, true, filePaths).distinct
+		for (path_prin <- pathArray) { println(path_prin)}
+		var data = new ListBuffer[(String, Long)]
+		breakable {
+			for (path <- pathArray) {
+				increment = increment + 1
+				var pathSplit = path.toString().split("/")
+				for( path1 <- pathSplit) {println(path1)}
+				var pathSplitSize = pathSplit.size
+				var basepath_partition = base_path
+				for (i <- nbrkeys to 1 by -1) {
+					basepath_partition = basepath_partition + "/" + pathSplit(pathSplitSize - i)
+				}
+				val MEGABYTE: Long = 1024L * 1024L
+				val folder_size = fs.getContentSummary(new Path(basepath_partition)).getLength / MEGABYTE
+				val last_partition_level = pathSplit(pathSplitSize - 1)
+				data += ((last_partition_level, folder_size))
+				logger.info("last_partition_level, folder_size" + (last_partition_level, folder_size))
+				//var processFolderName = target_path + "/" + pathSplit(pathSplitSize - 2)
+				logger.info(path)
+				logger.info(folder_size)
+				if (increment > 500) break // break out of the for loop
+				//fs.rename(new Path(rawFileName), new Path(processFileName))
 			}
-			match {
-				case Success(v) =>
-					logger.info("The data successfully inserted into  -----> " + hiveDB + "." + Outputhive_table)
-					insertFlag = true
-				//break the loop.
-				case Failure(e: Throwable) =>
-					logger.error("Error occurred while inserting the data  -----> " + hiveDB + "." + Outputhive_table + " Insert attempt number " + insrt_countr.value + " of 3. Error message: " + e.printStackTrace())
-					logger.info("The schema for "+ hiveDB + "." + Outputhive_table + " " + data_df.printSchema())
-					Thread.sleep(start + rnd.nextInt( (end - start) + 1 ))
+			import spark.implicits._
+			var DF = spark.sparkContext.parallelize(data).toDF("last_partition_level","folder_size")
+			DF = DF.groupBy("last_partition_level").agg(mean(col("folder_size")).as("folder_size") )
+			DF.show(false)
+			val sublevel_partitions = DF.select(col("last_partition_level")).distinct()  // to get states in a df
+			val sublevel_partition_list = sublevel_partitions.rdd.map(x=>x(0)).collect.toList //to get states in a list
+
+
+
+			for (sublevel_partition <- sublevel_partition_list)  //loop to get each state
+			{
+				logger.info("sublevel_partition : " + (sublevel_partition.toString splitAt (sublevel_partition.toString lastIndexOf "=") + 1)._2)
+				df.show(false)
+				val filtered_df = df.filter(col(partition_groupby) === (sublevel_partition.toString splitAt (sublevel_partition.toString lastIndexOf "=") +1)._2)
+				filtered_df.show(false)
+				filtered_df.printSchema()
+				var folder_size = DF.filter($"last_partition_level" === sublevel_partition).select("folder_size").collect().map(_.getDouble(0).ceil).mkString("")
+				val repartition_size = (folder_size.toDouble/512).ceil.toInt
+				logger.info("repartition_size : " + repartition_size)
+				val columns = partition_keys.toSeq.map(x => col(x))
+				filtered_df.coalesce(repartition_size).write.partitionBy((partition_keys.toSeq):_*).mode("overwrite").parquet(base_path + "_temp")
 			}
 		}
-		if (!insertFlag) {
-			logger.error("Error occurred while inserting the data  -----> " + hiveDB + "." + Outputhive_table + " Exceeded maximum number of retry attempts ")
-			val err_msg = "Error occurred while inserting the data  -----> " + hiveDB + "." + Outputhive_table + " Exceeded maximum number of retry attempts. Please check the spark log for details."
-			AuditIntegration.audit_final_call("1", "Failed", err_msg)
-			System.exit(1)
-		}
 	}
-
-
 }
